2d9e3ab64c491505b2ccbdc7abdac9b19fb5ee79
omega <soh95@naver.com>
2025-04-11 06:24:54 +0900

    feat: 프레젠테이션 레이어 포인트 관련 기능 추가 및 테스트 코드 작성

--- src/main/java/kr/hhplus/be/server/config/swagger/api/PointApi.java
+++ src/main/java/kr/hhplus/be/server/config/swagger/api/PointApi.java
@@ -3,11 +3,9 @@ package kr.hhplus.be.server.config.swagger.api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import kr.hhplus.be.server.api.point.presentation.controller.dto.request.ChargePointRequest;
-import kr.hhplus.be.server.api.point.presentation.controller.dto.response.ChargePointResponse;
-import kr.hhplus.be.server.api.point.presentation.controller.dto.response.FindPointHistoryResponse;
-import kr.hhplus.be.server.api.point.presentation.controller.dto.response.FindUserPointResponse;
-import kr.hhplus.be.server.common.response.CommonResponse;
+import kr.hhplus.be.server.presentation.point.PointRequest;
+import kr.hhplus.be.server.presentation.point.PointResponse;
+import kr.hhplus.be.server.presentation.common.response.CommonResponse;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,13 +15,13 @@ import java.util.List;
 public interface PointApi {
 
     @Operation(summary = "포인트 조회", description = "사용자의 현재 포인트를 조회합니다.")
-    CommonResponse<FindUserPointResponse> getUserPoint(
+    PointResponse.UserPoint getUserPoint(
             @Parameter(description = "유저 ID", example = "1")@PathVariable Long userId);
 
     @Operation(summary = "포인트 충전", description = "포인트를 충전합니다.")
-    CommonResponse<ChargePointResponse> chargePoint(@RequestBody ChargePointRequest request);
+    PointResponse.Charge chargePoint(@RequestBody PointRequest.Charge request);
 
     @Operation(summary = "포인트 내역 조회", description = "사용자의 포인트 사용 및 충전 내역을 조회합니다.")
-    CommonResponse<List<FindPointHistoryResponse>> findPointHistories(
+    List<PointResponse.History> findPointHistories(
             @Parameter(description = "유저 ID", example = "1")@PathVariable Long userId);
 }
