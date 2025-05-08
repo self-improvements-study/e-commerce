f99047cdda7514b65555e2fc33a5cffd248a80c0
omega <soh95@naver.com>
2025-05-08 15:08:26 +0900

    feat: 주문 분산락 적용

--- src/main/java/kr/hhplus/be/server/application/order/OrderCriteria.java
+++ src/main/java/kr/hhplus/be/server/application/order/OrderCriteria.java
@@ -1,14 +1,12 @@
 package kr.hhplus.be.server.application.order;
 
 import jakarta.annotation.Nullable;
-import kr.hhplus.be.server.presentation.order.OrderRequest;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class OrderCriteria {
@@ -18,6 +16,10 @@ public final class OrderCriteria {
     public static class Detail {
         private long userId;
         private List<Item> items;
+
+        public List<Long> toOptionIds() {
+            return this.items.stream().map(Item::getOptionId).sorted().toList();
+        }
     }
 
     @Getter
