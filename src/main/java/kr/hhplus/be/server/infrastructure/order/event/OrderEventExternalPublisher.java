35c439f55c54639b6b0c5db1dd8c56880d4b3c14
omega <soh95@naver.com>
2025-06-06 02:36:46 +0900

    feat: 부하 테스트를 위한 Grafana, Influxdb 설정 및 코드 수정

--- src/main/java/kr/hhplus/be/server/infrastructure/order/event/OrderEventExternalPublisher.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/order/event/OrderEventExternalPublisher.java
@@ -3,9 +3,11 @@ package kr.hhplus.be.server.infrastructure.order.event;
 import kr.hhplus.be.server.domain.order.OrderEvent;
 import kr.hhplus.be.server.domain.order.OrderEventPublisher;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component
 @RequiredArgsConstructor
 public class OrderEventExternalPublisher implements OrderEventPublisher {
